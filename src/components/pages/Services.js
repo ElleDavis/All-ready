271f2dc81a264a05341589c53a2e88d1ad954fc2
Elle Davis <ShanitriceDavis@gmail.com>
2021-05-06 14:47:34 -0400

    card changes

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -8,7 +8,7 @@ export default function Services() {
     return(
         <div className="services">
             <h1>Services</h1>
-            <h2>need feature after sign in</h2>
+            <h2>Looking ahead to Summer 21'</h2>
             
             
         </div>
